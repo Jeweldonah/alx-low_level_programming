233916be9e71ffb07622a7f04fd3a84bf216f2c3
Jeweldonah <jeweldonah1@gmail.com>
2023-02-21 06:08:11 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * print_alphabet - prints the alphabets
+ * Description: a function that prints letters in lower case
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char (letter = 'a')
+
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		letter++;
+	}
+	_putchar('\n');
+}
+
