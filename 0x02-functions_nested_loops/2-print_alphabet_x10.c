24c9988c5d194da36e72064a9c7f8bd5cada035e
Jeweldonah <jeweldonah1@gmail.com>
2023-02-21 09:16:19 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,9 @@
 #include "main.h"
 /**
- * print_alphabet_x10 - Prints the alphabet 10 times.
+ * print_alphabet_x10 - Prints the alphabet
+ * Description: prints alphabet 10 times
  *
- * Return: Always 0
+ * Return: 0
  */
 void print_alphabet_x10(void);
 {
@@ -11,7 +12,7 @@ void print_alphabet_x10(void);
 
 	for (i = 0; i <= 9; i++)
 	{
-		for (c = 'a'; c <= 'z'; c++)
+		for (c = 'a'; c <= 'z'; ic++)
 		{
 		_putchar(c);
 		}
