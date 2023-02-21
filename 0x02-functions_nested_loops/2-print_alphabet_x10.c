ed02930a0ec05123b3d4fc5b762507339301af6b
Jeweldonah <jeweldonah1@gmail.com>
2023-02-21 09:03:43 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Prints the alphabet 10 times.
+ *
+ * Return: Always 0
+ */
+void print_alphabet_x10(void);
+{
+	int i;
+	char c;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+		{
+		_putchar(c);
+		}
+	_putchar('\n');
+	}
+}
