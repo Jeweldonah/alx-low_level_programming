e57c7a0ff8f126fd4e65078270def27967d40f26
Jeweldonah <jeweldonah1@gmail.com>
2023-02-21 05:25:41 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,13 @@
 #include <stdio.h>
+
 /**
  * main - print _putchar, followed by a new line
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	printf("_putchar\n");
 
 	return (0);
 }
