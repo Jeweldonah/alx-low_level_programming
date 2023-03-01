3c7b67b21ddf27b723f4edd17198e8187df68218
Jeweldonah <jeweldonah1@gmail.com>
2023-03-01 11:41:32 -0800

    2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strcpy - function that copies a string
+ * @src: string to be copied
+ * @dest: string destination
+ * @n: number
+ * Return: void
+ */
+
+char *_strncpy(char *dest, char *src, int n);
+{
+	int i;
+
+	i = 0;
+
+	while (src[i] != '\0' && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+
+	return (dest);
+}
